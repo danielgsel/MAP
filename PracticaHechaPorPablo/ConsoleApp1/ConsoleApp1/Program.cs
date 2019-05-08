c18f2d616236520d0c27e2ee0f60df4e8f1038e5
Daniel <dangon21@ucm.es>
2019-05-08 12:08:46 +0200

    Pablo

---
+++ PracticaHechaPorPablo/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i < 50; ++i)
+            Console.WriteLine(i + ": " + EsPrimo(i));
+        }
+        public static bool EsPrimo(int i){
+        
+            if (i < 2) return false;
+            int p = 2;
+            while ((p * p <= i) && (i % p) != 0)
+            {
+                ++p;
+            }
+            return p * p > i;
+        }
+    }
+}
---
+++ PracticaHechaPorPablo/ConsoleApp1/Test2/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lista;
+using NUnit.Framework;
+
+namespace TestLista
+{
+    [TestFixture]
+    public class TestLista
+    {
+        [Test]
+
+        public void CuentaEltos()
+        {
+            Lista.Lista l = new Lista.Lista(3, 2);
+
+            Assert.AreEqual(6, l.cuentaEltos());
+        }
+
+        /*[Test]
+
+        public void InsertaFin()
+        {
+
+        }
+
+        [Test]
+
+        public void BorraElto()
+        {
+
+        }
+
+        [Test]
+
+        public void NEsimo()
+        {
+
+        }*/
+    }
+}
